469fe9e7804bc86564750c2605f3810d3f5e83a2
Haydn King <hjking734@gmail.com>
2011-08-16 17:06:54 +0100

    Made fragment JSON 'get' API for new-style fragment sequence display.

---
+++ gibthon/fragment/api.py
@@ -0,0 +1,91 @@
+"""These views are simply a JSON API for the fragment view"""
+
+from fragment.models import *
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound, Http404
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+
+import simplejson as json
+
+OK = 0
+ERROR = -1
+
+class JsonResponse(HttpResponse):
+	def __init__(self, data, state = OK):
+		HttpResponse.__init__(self, json.dumps([state, data]), mimetype='application/json')
+
+get_values = ['meta', 'seq', 'annotations', 'refs', 'feats']
+
+@login_required
+def get(request, _id):
+	"""Handles a JS request for data"""
+	try:
+		the_id = int(_id)
+	except ValueError:
+		raise Http404
+		
+	#if request.is_ajax() and 'value' in request.GET:
+	if 'value' in request.GET:
+		value = request.GET['value'].lower()
+		if not value in get_values:
+			return JsonResponse("ERROR: Invalid value '%s'." % value, ERROR)
+		try:
+			g = Gene.objects.get(id = the_id, owner=request.user)
+		except ObjectDoesNotExist:
+			return JsonResponse("ERROR: Fragment with ID='%s' does not exist." % id, ERROR)
+		
+		if value == 'meta':
+			data = {	'name': g.name,
+						'desc': g.description,
+					 }
+		elif value == 'seq':
+			#return a section of the sequence
+			try:
+				offset = int(request.GET.get('offset', 0))
+			except ValueError:
+				return JsonResponse("ERROR: Invalid offset '%s'." % request.GET.get('offset', 0), ERROR)
+			try:
+				length = int(request.GET.get('length', 1000))
+			except ValueError:
+				return JsonResponse("ERROR: Invalid length '%s'." % request.GET.get('length', 1000), ERROR)
+			data = g.sequence[offset : offset+length]
+		elif value == 'annotations':
+			#return a dict of the annotations
+			data = {}
+			for a in g.annotations.all():
+				if a.key not in data:
+					data[a.key] = []
+				data[a.key].append(a.value)
+		elif value == 'refs':
+			#get references
+			data = []
+			for r in g.references.all():
+				data.append({	'title': r.title,
+									'authors':r.authors,
+									'journal':r.journal,
+									'medline_id':r.medline_id,
+									'pubmed_id':r.pubmed_id,
+								})
+		elif value == 'feats':
+			#get features
+			data = []
+			for f in g.features.all():
+				quals = []
+				for q in f.qualifiers.all():
+					quals.append({	'name': q.name,
+										'data': q.data,
+									 })
+				s = None
+				if f.direction == 'f':
+					s = 1
+				elif f.direction == 'r':
+					s = -1
+				data.append({	'start': f.start,
+									'end': f.end,
+									'strand': s,
+									'type': f.type,
+									'qualifiers': quals,
+								})
+		#all went well, return the JSON
+		return JsonResponse(data)
+	raise Http404
