99a0bd7f2b11e82903e4ac6be9d1803b8e915ec2
Haydn King <hjking734@gmail.com>
2012-01-05 19:19:30 +0000

    Added basic tweening

--- gibthon/gibson/static/js/designer.js
+++ gibthon/gibson/static/js/designer.js
@@ -1,105 +1,102 @@
-(function( $, undefined ) {
+var canvas;
+var stage;
 
-var colnum = 0;
 
-var hsv_2_rgb = function(h,s,v)
+//Declare layout values
+//constants
+var LIB_HANDLE_WIDTH = 20;
+var LIB_STOPPER_WIDTH = 6;
+//variable
+var bounds = new Rectangle();
+var LIB_PANEL_CLOSED = 0;
+var LIB_PANEL_OPEN = 0;
+var LIB_SLIDER_OPEN = 0;
+
+var UI_BG_FILL = "rgb(100,100,100)";
+var UI_BG_STROKE = "rgb(20,20,20)";
+var UI_FG_FILL = "#DFEFFC";
+var UI_FG_STROKE = "#C5DBEC"
+
+var library;
+var slider;
+var stop;
+
+var init_designer = function(){	
+	var $c = $('#cdesigner');
+	$c.prop('width', $c.width());
+	$c.prop('height', $c.height());
+	canvas = document.getElementById('cdesigner');
+	stage = new Stage(canvas);
+	stage.enableMouseOver();
+	
+	_calc_size();
+	
+	//Build object hirachy and set initial positions
+	var hnd = new Shape(_build_handle());
+	stop = new Shape(_build_stopper());
+	stop.x = self.LIB_HANDLE_WIDTH;
+	var slider = new Container();
+	slider.addChild(hnd);
+	//slider.addChild(itemPanel);
+			
+	library = new Container();
+	library.addChild(slider);
+	library.addChild(stop);
+	library.x = LIB_PANEL_CLOSED;
+	library.onMouseOver = function(event) 
+	{
+		var target = {x: LIB_SLIDER_OPEN,};
+		var tween = Tween.get(slider)
+			.to(target, 500, Ease.quartOut);
+	};
+	library.onMouseOut = function(event)
+	{
+		var target = {x: 0,};
+		var tween = Tween.get(slider)
+			.to(target, 500, Ease.quartOut);
+	};
+	
+	
+	stage.addChild(library);
+	
+	Ticker.setFPS(25);
+	Ticker.addListener(this);
+};
+var tick = function()
 {
-	var r, g, b;
+	//console.log('tick');
+	stage.update();
+};
+var _calc_size = function(){
+	bounds.width = canvas.width;
+	bounds.height = canvas.height;
+	LIB_PANEL_CLOSED = bounds.width - LIB_HANDLE_WIDTH - LIB_STOPPER_WIDTH;
+	LIB_PANEL_OPEN = 0.618 * bounds.width;
+	LIB_SLIDER_OPEN = LIB_PANEL_OPEN - LIB_PANEL_CLOSED;
+	console.log(bounds.width + 'x' + bounds.height);
+};
 
-	if(s==0)
-	{
-		r=g=b=Math.round(v*255);
-	}
-	else
-	{
-		// h must be < 1
-		var var_h = h * 6;
-		if (var_h==6) var_h = 0;
-		//Or ... var_i = floor( var_h )
-		var var_i = Math.floor( var_h );
-		var var_1 = v*(1-s);
-		var var_2 = v*(1-s*(var_h-var_i));
-		var var_3 = v*(1-s*(1-(var_h-var_i)));
-		if(var_i==0)
-		{ 
-			var_r = v; 
-			var_g = var_3; 
-			var_b = var_1;
-		}
-		else if(var_i==1)
-		{ 
-			var_r = var_2;
-			var_g = v;
-			var_b = var_1;
-		}
-		else if(var_i==2)
-		{
-			var_r = var_1;
-			var_g = v;
-			var_b = var_3
-		}
-		else if(var_i==3)
-		{
-			var_r = var_1;
-			var_g = var_2;
-			var_b = v;
-		}
-		else if (var_i==4)
-		{
-			var_r = var_3;
-			var_g = var_1;
-			var_b = v;
-		}
-		else
-		{ 
-			var_r = v;
-			var_g = var_1;
-			var_b = var_2
-		}
-		//rgb results = 0 ÷ 255  
-		r=Math.round(var_r * 255);
-		g=Math.round(var_g * 255);
-		b=Math.round(var_b * 255);
-	}
-	return {r:r, g:g, b:b}
-}
-
-var get_col = function(){
-	var h = (colnum * (5 / 7)) % 1;
-	var s = 0.7;
-	var v = 1.0;
+//Graphics Builders
+var _build_handle = function()
+{
+	var g = new Graphics();
+	g.setStrokeStyle(1);
+	g.beginFill(UI_BG_FILL);
+	g.beginStroke(UI_BG_STROKE);
+	g.drawRect(LIB_HANDLE_WIDTH, 8, LIB_PANEL_OPEN, bounds.height - 16);
 	
-	var col = hsv_2_rgb(h,s,v);
+	g.beginFill(UI_FG_FILL);
+	g.beginStroke(UI_FG_STROKE);
+	g.drawRoundRectComplex( 0, 0, LIB_HANDLE_WIDTH, bounds.height, 4, 0, 0, 4);
 	
-	colnum = colnum + 1;
-	return 'rgb('+col.r+','+col.g+','+col.b+')';
-}
-
-var PI2 = 2 * Math.PI;
-
-
-$.widget("ui.designer", {
-	options: {
-		name: '',
-		id: 0,
-		titleFont: '20px bold Arial',
-		titleColour: 'rgb(20,20,20)',
-		lengthFont: '16px Arial',
-		lengthColour: 'rgb(100,100,100)',
-	},
-	_init: function(){
-	},
-	_create: function(){		
-		var self = this;
-		this.canvas = this.element[0];
-	},
-// ------------------------------------------------------------------------------------ PUBLIC API
-	addFragment: function(id, tell_server, redraw){	
-	},
-	changeName: function(new_name){
-	},
-// ---------------------------------------------------------------------------------- PRIVATE API
-
-});
-
-})(jQuery);
+	return g;
+};
+var _build_stopper = function()
+{
+	var g = new Graphics();
+	g.setStrokeStyle(1);
+	g.beginFill(UI_FG_FILL);
+	g.beginStroke(UI_FG_STROKE);
+	g.drawRoundRectComplex( 0, 0, LIB_STOPPER_WIDTH, bounds.height, 0, 4, 4, 0);
+	return g;
+};
