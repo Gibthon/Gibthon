650faa6dbfaf149ecd73117cae652a63b883b497
Haydn King <hjking734@gmail.com>
2012-01-07 17:01:34 +0000

    Library now more object orientated

--- gibthon/gibson/static/js/designer.js
+++ gibthon/gibson/static/js/designer.js
@@ -8,11 +8,12 @@ var LIB_HANDLE_WIDTH = 20;
 var LIB_STOPPER_WIDTH = 6;
 var LIB_ITEM_HEIGHT = 20;
 var LIB_ITEM_SPACING = 4;
+
+var FRAG_W = 25;
+
 //variable
 var bounds = new Rectangle();
-var LIB_PANEL_CLOSED = 0;
-var LIB_PANEL_OPEN = 0;
-var LIB_SLIDER_OPEN = 0;
+var LIB_X = 0; var LIB_Y = 0; var LIB_W = 0; var LIB_H; var LIB_SLIDE = 0;
 
 //colours
 var UI_BG_FILL 				= Graphics.getRGB(108,165,208);
@@ -35,22 +36,324 @@ var UI_TEXT 				= Graphics.getRGB(46,110,158);
 var UI_TEXT_HL 				= Graphics.getRGB(29,89,135);
 var UI_TEXT_SL 				= Graphics.getRGB(225,112,9);
 
-var state = 'd';
 /*
- * d - default
- * dr - drag
- * 
+ * Fragment - inherits display object and represents a fragment which is in view
+ *  
  * */
+Fragment.prototype = new Shape();
+Fragment.prototype.constructor = Fragment;
+function Fragment(metadata)
+{
+	var self = this;
+	Shape.call(this, new Graphics);
+	this.meta = metadata;
+	this.size = 0;//in degrees
+	this.dragging = false;
+	this.area = 'rm'; //or ccw or cw
+	
+	var anim = 0; //the number of animations currently going on, controls whether to redraw
+	
+	this.setAnim = function(){anim = anim+1;}
+	this.clearAnim = function(){anim=anim-1; if(anim==0) self.redraw();}
+	
+	// -------------- Transitions
+	this.scale = function(s) {};
+	this.rotate = function(a) {};
+	this.drag = function(d) {};
+	this.move = function(a) {};
+	// -------------- end Transitions
+	
+	
+	this.redraw = function()
+	{
+		var g = self.graphics.clear();
+		switch(self.area)
+		{
+			case 'rm':
+				g.beginFill(UI_FG_FILL)
+				 .beginStroke(UI_FG_STROKE);
+				 .rect(-FRAG_W * 3, -FRAG_W * .5, FRAG_W * 6, FRAG_W * .5);
+				break;
+			case 'cw':
+				break;
+			case 'ccw':
+				break;
+		}
+	};
+	this.tick = function()
+	{
+		if(anim > 0)
+			self.redraw();
+		if(self.dragging)
+		{
+			switch(self.area)
+			{
+				case 'rm':
+					self.x = stage.mouseX;
+					self.y = stage.mouseY;
+					break;
+				case 'cw':
+				case 'ccw':
+					
+			}
+		}
+	}
+	this.redraw();
+};
+
+/*
+ * Item - a fragment as it appears in the library
+ *  
+ * */
+Item.prototype = new Container();
+Item.prototype.constructor = Item;
+function Item(x,y,w,h,m) //x,y,w,h, metadata
+{
+	var self = this;
+	Container.call(this);
+	
+	this.fill = UI_FG_FILL;
+	this.stroke = UI_FG_STROKE;
+	this.meta = m;
+	this.x = x;
+	this.y = y;
+	
+	var t = new Text(m.name, '500 12px Lucida Grande,Lucida Sans,Arial,sans-serif', UI_TEXT);
+	t.textAlign = 'center';
+	t.textBaseline = 'middle';
+	t.x = w / 2.0;
+	t.y = h / 2.0;
+	var shape = new Shape(new Graphics);
+	var r = new Rectangle(x,y,w,h);
+	this.addChild(shape);
+	this.addChild(t);
+	
+	this.onMouseMove = function(x,y) //return true if mouse is over me
+	{
+		if(r.containsXY(x,y))
+		{
+			//console.log('Mouse In: ' + self.meta.name);
+			self.fill = UI_FG_FILL_HL;
+			self.stroke = UI_FG_STROKE_HL;
+			_set_cursor('move');
+			self.mouseOver = true;
+			_redraw();
+			stage.update();
+			return true;
+		}
+		else if(self.mouseOver)
+		{
+			//console.log('Mouse Out: ' + self.meta.name);
+			self.fill = UI_FG_FILL;
+			self.stroke = UI_FG_STROKE;
+			_set_cursor('auto');
+			self.mouseOver = false;
+			_redraw();
+			stage.update();
+		}
+		return false;
+	};	
+			
+	var _redraw = function()
+	{
+		var g = shape.graphics;
+		g.clear()
+		 .beginFill(self.fill)
+		 .beginStroke(self.stroke)
+		 .drawRoundRect(0,0,w,h,4);		
+	}
+	
+	_redraw();
+};
+
+/*
+ * Library - the library
+ *  
+ * */
+Library.prototype = new Container();
+Library.prototype.constructor = Library;
+function Library(x,y,w,h, slide)
+{
+	var self = this;
+	Container.call(this);
+	
+	this.open = false;
+	this.slide = slide;
+	this.onSelect = null;
+	this.x = x;
+	this.y = y;
+	this.width = w;
+	this.height = h;
+	
+	var itemPanel;
+	var itemHeight;
+	
+	//PUBLIC METHODS	
+	this.Open = function() 
+	{
+		self.open = true;
+		var target = {x: -self.slide,};
+		var tween = Tween.get(slider)
+			.call(setAnim)
+			.to(target, 500, Ease.quartOut)
+			.call(clearAnim);
+	};
+	this.Close = function()
+	{
+		self.open = false;
+		var target = {x: 0,};
+		var tween = Tween.get(slider)
+			.call(setAnim)
+			.to(target, 500, Ease.quartOut)
+			.call(clearAnim);
+	};
+	this.addItem = function(m, update) //addItem(metadata, update = true)
+	{
+		var i = new Item(0,itemHeight,itemPanel.width,LIB_ITEM_HEIGHT,m);
+		itemHeight = itemHeight + LIB_ITEM_HEIGHT + LIB_ITEM_SPACING;
+		itemPanel.addChild(i);
+		if(update == undefined) update = true;
+		if(update)
+			stage.update();
+	};
+	this.addItems = function(m) //addItems(Array[metadata])
+	{
+		for(i in m) self.addItem(m[i], false);
+		stage.update();
+	}
+	this.onMouseMove = function(e)
+	{
+		//console.log('library.onMouseMove() ('+e.stageX+','+e.stageY+')');
+		if(self.open)
+		{
+			if(!stage.mouseInBounds)
+			{
+				self.Close();
+				return;
+			}
+			if(!rc.containsXY(e.stageX, e.stageY))
+			{
+				self.Close();
+				return;
+			}
+			else
+			{
+				//are we moused over any items?
+				var l = itemPanel.globalToLocal(e.stageX, e.stageY);
+				for(var i = 0; i < itemPanel.getNumChildren(); i = i + 1)
+				{
+					itemPanel.getChildAt(i).onMouseMove(l.x,l.y);
+				}
+			}
+		}
+		if(!self.open)
+		{
+			if(ro.containsXY(e.stageX, e.stageY))
+			{
+				self.Open();
+				return;
+			}
+		}
+	};
+	self.onMouseDown = function(e)
+	{
+		if(self.open)
+		{
+			for(var i = 0; i < itemPanel.getNumChildren(); i = i + 1)
+			{
+				if(itemPanel.getChildAt(i).mouseOver)
+				{
+					self.Close();
+					self.onSelect(e, itemPanel.getChildAt(i).meta);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	//PRIVATE METHODS
+	var _build_handle = function(handle_w, panel_w, h)
+	{
+		var g = new Graphics();
+		//Draw the panel
+		g.setStrokeStyle(1);
+		g.beginFill(Graphics.getRGB(0,0,0,0.8));
+		g.drawRect(handle_w, 8, panel_w, h - 16);
+		
+		//Draw the Handle
+		g.beginFill(UI_BG_FILL);
+		g.beginStroke(UI_BG_STROKE);
+		g.drawRoundRectComplex( 0, 0, LIB_HANDLE_WIDTH, bounds.height, 4, 0, 0, 4);
+		
+		var s = new Shape(g);	
+		
+		//add the text
+		var t = new Text('Library', '700 13.33px Lucida Grande,Lucida Sans,Arial,sans-serif', UI_FG_FILL);
+		t.textBaseline = 'middle';
+		t.rotation = -90;
+		t.x = LIB_HANDLE_WIDTH / 2.0;
+		t.y = bounds.height * 0.2; 
+		
+		var c = new Container();
+		c.addChild(s);
+		c.addChild(t);
+		
+		return c;
+	};
+		
+	var _build_stopper = function(x,y,w,h)
+	{
+		var g = new Graphics();
+		g.setStrokeStyle(1);
+		g.beginFill(UI_BG_FILL);
+		g.beginStroke(UI_BG_STROKE);
+		g.drawRoundRectComplex( 0, 0, w, h, 0, 4, 4, 0);
+		
+		var s = new Shape(g);
+		s.x = x;
+		s.y = y;
+		return s;
+	};
+	var _build_item_panel = function(x,y,w,h)
+	{
+		var r = new Container();
+		r.x = x;
+		r.y = y;
+		r.height = h;
+		r.width = w;
+		return r;
+	}
+	
+	
+	//initiation object hirachy
+	var hnd = _build_handle(LIB_HANDLE_WIDTH, self.slide, self.height);
+	var stop = _build_stopper(LIB_HANDLE_WIDTH, 0, LIB_STOPPER_WIDTH, self.height);
+	itemPanel = _build_item_panel(LIB_HANDLE_WIDTH + 4, 10, self.slide - 16, self.height - 10);
+	itemHeight = LIB_ITEM_SPACING;
+	
+	var ro = new Rectangle(x,y,w,h);
+	var rc = new Rectangle(x-this.slide,y,w+this.slide,h);
+	
+	var slider = new Container();
+	slider.x = 0;
+	slider.y = 0;
+	slider.height = this.height;
+	slider.width = this.slide;
+	slider.addChild(hnd);
+	slider.addChild(itemPanel);
+			
+	this.addChild(slider);
+	this.addChild(stop);
+	
+}; //end Library def
 
 var library;
-var slider;
-var stop;
-var itemPanel;
+
 var $c;
 //stage is only updated if an animation is playing
 var anim = 0;
 var setAnim = function() {anim=anim+1;};
-var clearAnim = function() {setTimeout('anim=anim-1;', 500);};
+var clearAnim = function() {anim=anim-1; if(anim == 0) stage.update();};
 
 var init_designer = function(){	
 	$c = $('#cdesigner');
@@ -62,25 +365,12 @@ var init_designer = function(){
 	
 	_calc_size();
 	
-	//Build object hirachy and set initial positions
-	var hnd = _build_handle();
-	stop = _build_stopper();
-	stop.x = self.LIB_HANDLE_WIDTH;
-	itemPanel = new Container();
-	itemPanel.x = LIB_HANDLE_WIDTH + 4;
-	itemPanel.y = 10;
-	itemPanel.height = bounds.height - 10;
-	itemPanel.width = (bounds.width - LIB_PANEL_OPEN) - LIB_HANDLE_WIDTH - LIB_STOPPER_WIDTH - 8;
-	slider = new Container();
-	slider.addChild(hnd);
-	slider.addChild(itemPanel);
-			
-	library = new Container();
-	library.addChild(slider);
-	library.addChild(stop);
-	library.x = LIB_PANEL_CLOSED;
-	library.open = false;
-		
+	
+	library = new Library(LIB_X,LIB_Y,LIB_W,LIB_H,LIB_SLIDE);
+	library.onSelect = function(e, m)
+	{
+		console.log('Selected: ' + m.name);
+	}
 	stage.addChild(library);
 	
 	stage.onMouseMove = _on_mouse_move;
@@ -99,221 +389,69 @@ var tick = function()
 var _calc_size = function(){
 	bounds.width = canvas.width;
 	bounds.height = canvas.height;
-	LIB_PANEL_CLOSED = Math.floor(bounds.width - LIB_HANDLE_WIDTH - LIB_STOPPER_WIDTH);
-	LIB_PANEL_OPEN = Math.floor(0.618 * bounds.width);
-	LIB_SLIDER_OPEN = LIB_PANEL_OPEN - LIB_PANEL_CLOSED;
+	LIB_X = Math.floor(bounds.width - LIB_HANDLE_WIDTH - LIB_STOPPER_WIDTH);
+	LIB_Y = 0;
+	LIB_SLIDE = Math.floor((1-0.618 ) * bounds.width)
+	LIB_W = LIB_HANDLE_WIDTH + LIB_STOPPER_WIDTH;
+	LIB_H = bounds.height;
 };
 
 var show_items = function(metadata)
 {
-	for(var i in metadata)
-	{
-		var s = new Rectangle(0, i * (LIB_ITEM_HEIGHT + LIB_ITEM_SPACING),itemPanel.width, LIB_ITEM_HEIGHT);
-		var item = _draw_item(metadata[i].name, s, 'n');
-		item.meta = metadata[i];
-		item.onMouseOver = function(event)
-		{
-			console.log('item.onMouseOver()');
-			_redraw_item(item, 'hl');
-		}
-		item.onMouseOut = function(event)
-		{
-			console.log('item.onMouseOut()');
-			_redraw_item(item, 'n');
-		}
-		itemPanel.addChild(item);
-	}
+	library.addItems(metadata);
 	stage.update();
 };
-
-//Graphics Builders
-var _build_handle = function()
-{
-	var g = new Graphics();
-	g.setStrokeStyle(1);
-	g.beginFill(Graphics.getRGB(0,0,0,0.8));
-	g.drawRect(LIB_HANDLE_WIDTH, 8, LIB_PANEL_OPEN, bounds.height - 16);
-	
-	g.beginFill(UI_BG_FILL);
-	g.beginStroke(UI_BG_STROKE);
-	g.drawRoundRectComplex( 0, 0, LIB_HANDLE_WIDTH, bounds.height, 4, 0, 0, 4);
-	
-	var s = new Shape(g);	
-	
-	var t = new Text('Library', '700 13.33px Lucida Grande,Lucida Sans,Arial,sans-serif', UI_FG_FILL);
-	t.rotation = -90;
-	t.textBaseline = 'middle';
-	t.x = LIB_HANDLE_WIDTH / 2.0;
-	t.y = bounds.height * 0.2; 
-	
-	var c = new Container();
-	c.addChild(s);
-	c.addChild(t);
-	
-	return c;
-};
-var _build_stopper = function()
-{
-	var g = new Graphics();
-	g.setStrokeStyle(1);
-	g.beginFill(UI_BG_FILL);
-	g.beginStroke(UI_BG_STROKE);
-	g.drawRoundRectComplex( 0, 0, LIB_STOPPER_WIDTH, bounds.height, 0, 4, 4, 0);
-	return new Shape(g);
-};
-var _draw_item = function(name, size, state)
-{
-	var text = new Text(name, 'bold 12px arial', UI_TEXT_HL);
-	var shape = new Shape(new Graphics());	
-	
-	text.x = size.width / 2.0;
-	text.y = size.height / 2.0;
-	text.textAlign = 'center';
-	text.textBaseline = 'middle';
-	
-	var item = new Container();
-	item.addChild(shape);
-	item.addChild(text);
-	item.state = undefined;
-	item.x = size.x;
-	item.y = size.y;
-	item.width = size.width;
-	item.height = size.height;
-	return _redraw_item(item, state);
-};
-var _redraw_item = function(item, state)
-{
-	if(item.state == state) return;
-	item.state = state;
-	var g = item.getChildAt(0).graphics.clear();
-	var text = item.getChildAt(1);
-	g.setStrokeStyle(1);
-	
-	switch(state)
-	{
-		case 'hl':
-			text.color = UI_TEXT_HL;
-			g.beginStroke(UI_FG_STROKE_SL);
-			g.beginFill(UI_FG_FILL_SL);
-			_set_cursor('move');
-			break;
-		case 'sl':
-			text.color = UI_TEXT_SL;
-			g.beginStroke(UI_FG_STROKE_SL);
-			g.beginFill(UI_FG_FILL_SL);
-			_set_cursor('move');
-			break;
-		default:
-			text.color = UI_TEXT;
-			g.beginStroke(UI_FG_STROKE);
-			g.beginFill(UI_FG_FILL);
-			_set_cursor('auto');
-			break;
-	}
-	
-	g.drawRoundRect(0, 0, item.width, item.height, 4);
-	return item;
-}
 
 // Mouse things --------------------------
 var _on_mouse_move = function(e)
 {
-	if(!stage.mouseInBounds)
-	{
-		_closeLib();
-		return;
-	}
-	//should the library be opened?
-	if(!library.open && e.stageX > LIB_PANEL_CLOSED)
-		_openLib();
-	if(library.open)
-	{
-		if(e.stageX < LIB_PANEL_OPEN)
-			_closeLib();
-		else
-		{
-			//update states
-			var item; var ht;
-			for(var i = 0; i < itemPanel.getNumChildren(); i = i + 1)
-			{
-				item = itemPanel.getChildAt(i);
-				ht = _is_over(item, e.stageX, e.stageY);
-				
-				if(ht && item.state != 'hl')
-				{
-					_redraw_item(item, 'hl');
-					stage.update();
-				}
-				if(!ht && item.state == 'hl')
-				{	
-					_redraw_item(item, 'n');
-					stage.update();
-				}
-			}
-			
-		}
-	}
+	if(drag == undefined) //pass to the library
+		library.onMouseMove(e);
 };
 var _on_press = function(e)
 {
 	e.onMouseUp = _on_mouse_up;
 	e.onMouseMove = _on_mouse_drag;
-	if(library.open)
-	{
-		for(var i = 0; i < itemPanel.getNumChildren(); i = i + 1)
-		{
-			var item = itemPanel.getChildAt(i);
-			var h = _is_over(item, e.stageX, e.stageY);
-			if(h)
-				_begin_item_drag(item);
-		}
-	}
+	library.onMouseDown(e);
 }
 var _on_mouse_up = function(e)
 {
-	i_drag = undefined;
+	if(drag != undefined)
+	{
+		_stop_dragging(drag);
+		drag = undefined;
+	}
 	_set_cursor();
 }
 var _on_mouse_drag = function(e)
 {
-	if(i_drag != undefined)
-	{
-		i_drag.x = stage.mouseX - i_drag.width / 2.0;
-		i_drag.y = stage.mouseY - i_drag.height;
-		stage.update();
-	}
+	stage.update();
 }
 
-var _openLib = function() 
-{
-	library.open = true;
-	var target = {x: LIB_SLIDER_OPEN,};
-	var tween = Tween.get(slider)
-		.call(setAnim)
-		.to(target, 500, Ease.quartOut)
-		.call(clearAnim);
-};
-var _closeLib = function()
-{
-	library.open = false;
-	var target = {x: 0,};
-	var tween = Tween.get(slider)
-		.call(setAnim)
-		.to(target, 500, Ease.quartOut)
-		.call(clearAnim);
-};
-var i_drag;
+var drag;
 var _begin_item_drag = function(it)
 {
 	_closeLib();
-	state = 'dr';
-	i_drag = it.clone(true);
-	i_drag.width = it.width;
-	i_drag.height = it.height;
-	i_drag.x = stage.mouseX;
-	i_drag.y = stage.mouseY;
-	_redraw_item(i_drag, 'sl');
-	stage.addChild(i_drag);
+	drag = new Fragment(it.meta);
+	drag.drag = true;
+	stage.addChild(drag);
+	stage.update();
+}
+var _stop_dragging = function(d)
+{
+	switch(d.area)
+	{
+		case 'rm':
+			stage.removeChild(d);
+			d.drag = false;
+			break;
+		case 'cw':
+			d.drag = false;
+			break;
+		case 'ccw':
+			d.drag = false;
+			break;
+	}
 	stage.update();
 }
 
@@ -323,6 +461,17 @@ var _is_over = function(ob, x,y)
 {
 	var l = ob.globalToLocal(x,y);
 	return ob.hitTest(l.x,l.y);
+}
+Rectangle.prototype.containsPoint = function(p)
+{
+	return this.containsXY(p.x,p.y);
+}
+Rectangle.prototype.containsXY = function(x,y)
+{
+	return (x > this.x && 
+			x < (this.x + this.width) &&
+			y > this.y &&
+			y < (this.y + this.height));
 }
 //eg auto, move, wait
 var _set_cursor = function(type)
