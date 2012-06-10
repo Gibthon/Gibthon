d5d713132d2bbae25daffd1e6538c9f9af9896ac
Haydn King <hjking734@gmail.com>
2012-06-10 19:46:23 +0100

    Attempt to get AJAX to support gibthon error reporting

--- gibthon/static/js/ajax.js
+++ gibthon/static/js/ajax.js
@@ -55,8 +55,24 @@ var AJAX = new function()
 			args.xhr = function() {return makeUpdateXHR(update_function);};
 		}
 
-		$.ajax(args);
-	}
+		//replace the success function with my own
+		var s_fn = args.success;
+		args.success = function(data, textStatus, jqXHR)
+		{
+			//check for error 
+			if(data[0] == AJAX_ERROR)
+				{
+					//call the error callback, if it exists
+					if(args.error!=undefined)
+						args.error(data[1]);
+					else
+						console.error('AJAX Error: ' + data[1]);
+				}
+				else
+					s_fn(data[1]);
+
+				$.ajax(args);
+		}
 
 	var makeUpdateXHR = function(update_fn)
 	{
@@ -87,6 +103,12 @@ var AJAX = new function()
 		throw new Error("Could not create HTTP request object.");
 	}
 
+	/*
+	 * Private Variables
+	 */
+
+	//value returned when there's an error
+	var AJAX_ERROR = -1;
 }
 
 
