1d6d82806f17a360881a9f3f65f1a9e25ff63ca8
Haydn King <hjking734@gmail.com>
2012-08-22 23:38:51 +0100

    Switch to one-primer-at-a-time editing. #22

--- gibthon/gibson/static/js/libdesigner.js
+++ gibthon/gibson/static/js/libdesigner.js
@@ -1,9 +1,9 @@
 /*
-* libDesigner api
-*
-*   depends on ajax.js & libfrag.js
-*
-*/
+ * libDesigner api
+ *
+ *   depends on ajax.js & libfrag.js
+ *
+ */
 
 var libDesigner = new function()
 {
@@ -27,65 +27,65 @@ var libDesigner = new function()
             modified: 'blah',
             fs: [
                 {
-                    "origin": "BioBrick", 
-                    "name": "pSB1C3", 
-                    "refs": [], 
-                    "annots": {}, 
-                    "length": 2070, 
-                    "id": 1, 
-                    "desc": "High copy BioBrick assembly plasmid"
-                },
-                {
-                    "origin": "BioBrick", 
-                    "name": "BBa_K325219", 
-                    "refs": [], 
-                    "annots": {}, 
-                    "length": 3841, 
-                    "id": 2, 
-                    "desc": "Red Firefly Luciferase and LRE"+
-                        "(under pBAD)L. Cruciata(E. coli optimised)"
-                },
-                {
-                    "origin": "Nucleotide Database", 
-                    "name": "HD065425", 
-                    "refs": [], 
-                    "annots": {}, 
-                    "length": 4168, 
-                    "id": 5, 
-                    "desc": "Sequence 52 from Patent WO2010070295."
-                },
+                "origin": "BioBrick", 
+                "name": "pSB1C3", 
+                "refs": [], 
+                "annots": {}, 
+                "length": 2070, 
+                "id": 1, 
+                "desc": "High copy BioBrick assembly plasmid"
+            },
+            {
+                "origin": "BioBrick", 
+                "name": "BBa_K325219", 
+                "refs": [], 
+                "annots": {}, 
+                "length": 3841, 
+                "id": 2, 
+                "desc": "Red Firefly Luciferase and LRE"+
+                    "(under pBAD)L. Cruciata(E. coli optimised)"
+            },
+            {
+                "origin": "Nucleotide Database", 
+                "name": "HD065425", 
+                "refs": [], 
+                "annots": {}, 
+                "length": 4168, 
+                "id": 5, 
+                "desc": "Sequence 52 from Patent WO2010070295."
+            },
             ],
             cfs: [
                 {
-                    id: 1,
-                    direction: 1,
-                    s_offset: 0,
-	                s_feat: -1,
-                    e_offset: 0,
-                    e_feat: -1,
-                    order: 0,
-                },
-                {
-                    id: 2,
-                    direction: -1,
-                    s_offset: 0,
-	                s_feat: -1,
-                    e_offset: 0,
-                    e_feat: -1,
-                    order: 1,
-                },
-                {
-                    id: 3,
-                    direction: 1,
-                    s_offset: 0,
-	                s_feat: -1,
-                    e_offset: 0,
-                    e_feat: -1,
-                    order: 2,
-                },
+                id: 1,
+                direction: 1,
+                s_offset: 0,
+                s_feat: -1,
+                e_offset: 0,
+                e_feat: -1,
+                order: 0,
+            },
+            {
+                id: 2,
+                direction: -1,
+                s_offset: 0,
+                s_feat: -1,
+                e_offset: 0,
+                e_feat: -1,
+                order: 1,
+            },
+            {
+                id: 3,
+                direction: 1,
+                s_offset: 0,
+                s_feat: -1,
+                e_offset: 0,
+                e_feat: -1,
+                order: 2,
+            },
             ],
         };
-        
+
         return new Construct(test_data);
     };
 
@@ -108,7 +108,7 @@ function Construct(data)
         this.cfs.push(new ConstructFragment(data.cfs[i], f));
     }
 
-   this.addFragment = function(f, position, direction, _suc)
+    this.addFragment = function(f, position, direction, _suc)
     {
         console.log('AddingFragment at ' + position);
         AJAX.post({
@@ -160,182 +160,238 @@ function Construct(data)
 
 function ConstructFragment(d, f)
 {		
-	this.id = d.id;
-	this.strand = d.direction;
-	this.s_offset = d.s_offset;
-	this.s_feat = d.s_feat;
-	this.e_offset = d.e_offset;
-	this.e_feat = d.e_feat;
-	this.order = d.order;
-	this.f = f;
-	
-	this.startPos = function()
-	{
-		/*if(this.id == undefined) return 0;
-		//console.log('startPos: s_feat: '+this.s_feat+' s_offset: '+this.s_offset);
-		if(this.s_feat > 0)
-		{
-			//var sf = f.getFeatById(this.s_feat);
-			if(sf != null)
-				return this.s_offset + sf.start;
-		}*/
-		return this.s_offset;
-	};
-	this.endPos = function()
-	{
-		/*if(this.id == undefined) return this.f.getLength();
-		if(this.e_feat > 0)
-		{
-			var ef = this.f.getFeatById(this.e_feat);
-			if(ef != null)
-				return this.e_offset + ef.end;
-		}*/
-		return this.f.getLength() - this.s_offset;
-	};
-	this.getLength = function()
-	{
-		return Math.abs(this.endPos() - this.startPos());
-	};
+    this.id = d.id;
+    this.strand = d.direction;
+    this.s_offset = d.s_offset;
+    this.s_feat = d.s_feat;
+    this.e_offset = d.e_offset;
+    this.e_feat = d.e_feat;
+    this.order = d.order;
+    this.f = f;
+
+    this.startPos = function()
+    {
+        /*if(this.id == undefined) return 0;
+        //console.log('startPos: s_feat: '+this.s_feat+' s_offset: '+this.s_offset);
+        if(this.s_feat > 0)
+        {
+        //var sf = f.getFeatById(this.s_feat);
+        if(sf != null)
+        return this.s_offset + sf.start;
+        }*/
+        return this.s_offset;
+    };
+    this.endPos = function()
+    {
+        /*if(this.id == undefined) return this.f.getLength();
+          if(this.e_feat > 0)
+          {
+          var ef = this.f.getFeatById(this.e_feat);
+          if(ef != null)
+          return this.e_offset + ef.end;
+          }*/
+        return this.f.getLength() - this.s_offset;
+    };
+    this.getLength = function()
+    {
+        return Math.abs(this.endPos() - this.startPos());
+    };
     this.length = function()
     {
         console.warn('ConstructFragment.length depreciated');
         return this.getLength();
     }
-	this.toString = function() {return '[ConstructFragment (id='+this.id+') ]';};
- };
+    this.toString = function() {return '[ConstructFragment (id='+this.id+') ]';};
+};
 
- /*
-  * jQuery widget for previewing the designed fragment
-  */
- $.widget('ui.constructPreview', {
-     options: {
+/*
+ * * jQuery widget for previewing the designed fragment
+ * */
+$.widget('ui.constructPreview', {
+    options: {
 
-     },
-     _create: function() {
+    },
+    _create: function() {
         var self = this;
         var el = $(this.element[0]);
         this.el = el;
+        this.pview = el.find('.primer-view');
+        this.arrow = this.pview.find('.arrow');
 
         var css = $("<link>");
         css.attr({
-            rel:  "stylesheet",
+            rel: "stylesheet",
             type: "text/css",
             href: "/static/css/construct_preview.css"
         });
         $("head").append(css);
 
+        el.find('.primer').click( function(){
+            if($(this).hasClass('sl'))
+            {
+                el.find('.primer.sl').removeClass('sl');
+                self.pview.slideUp();
+                return;
+            }
+            el.find('.primer.sl').removeClass('sl');
+            $(this).addClass('sl');
+            self.arrow.css({'left': self._get_center($(this)), });
+            self.pview.slideDown();
+            self._set_colors($(this));
 
-        el.find('.fragment > .pcr-target').mouseenter(function() {
-            $(this).siblings('.fwd-primer').addClass('sl');
-            $(this).siblings('.rev-primer').addClass('sl');
-            $(this).parent().addClass('sl');
-        }).mouseleave(function() {
-            $(this).siblings('.fwd-primer').removeClass('sl');
-            $(this).siblings('.rev-primer').removeClass('sl');
-            $(this).parent().removeClass('sl');
-        }).click( function() {
-            var f = $(this).parent();
-            f.parent().find('.hl-target, .fragment').removeClass('sl');
-            $(this).siblings('.pcr-target-sl').addClass('sl');
-            self.showPrimerView($(this));
-        });
+            var type;
+            $(this).hasClass('fwd-primer') ? type = 'fwd' : type = 'rev';
 
-        el.find('.fragment > .join-target').mouseenter(function(){
-            var f = $(this).parent();
-            self._get_prev(f).find('.rev-primer').addClass('sl');
-            f.find('.fwd-primer').addClass('sl');
-            $(this).siblings('.join-target-hl').addClass('sl');
-        }).mouseleave(function()
-        {
-            var f = $(this).parent();
-            self._get_prev(f).find('.rev-primer').removeClass('sl');
-            f.find('.fwd-primer').removeClass('sl');
-            $(this).siblings('.join-target-hl').removeClass('sl');
-        }).click( function() {
-            var f = $(this).parent();
-            f.parent().find('.hl-target, .fragment').removeClass('sl');
-            $(this).siblings('.join-target-sl').addClass('sl');
-            self.showPrimerView($(this));
-        });
-
-
-        el.find('.fragment > .post-join-target').mouseenter(function()
-        {
-            $(this).siblings('.post-join-target-hl').addClass('sl');
-            var f = $(this).parent();
-            f.find('.rev-primer').addClass('sl');
-            self._get_next(f).find('.fwd-primer').addClass('sl');
-        }).mouseleave(function()
-        {
-            $(this).siblings('.post-join-target-hl').removeClass('sl');
-            var f = $(this).parent();
-            f.find('.rev-primer').removeClass('sl');
-            self._get_next(f).find('.fwd-primer').removeClass('sl');
-        }).click( function() {
-            console.log('post-join-target click');
-            var f = $(this).parent();
-            f.parent().find('.hl-target, .fragment').removeClass('sl');
-            console.log('  $(this).siblings(".post-join-target-sl").size() = ' + 
-                $(this).siblings(".post-join-target-sl").size());
-            $(this).siblings('.post-join-target-sl').addClass('sl');
-            self.showPrimerView($(this));
-        });
-
-        el.mouseleave( function() {
-            el.find('.fwd-primer').removeClass('sl');
-            el.find('.rev-primer').removeClass('sl');
-            el.find('.fragment').removeClass('sl');
-        });
-
-        el.find('button#cancel-btn').button({
-            'icons': {'primary': 'ui-icon-cancel',},
-        }).click( function() {
-            self.hidePrimerView();
-        });
-        el.find('button#save-btn').button({
-            'icons': {'primary': 'ui-icon-disk',},
-        }).click( function() {
-            self.hidePrimerView();
-        });
-     },
-     _init: function() {
-        this.el.find('.fragment:not(.bk-fragment)').each( function() {
-            $(this).find('.seq').css({
-                'background-color': libFrag.getNextColor(),
+            self.loadJson({    
+                'id': 95,
+                'flap_len': 20,
+                'flap_seq': 'ATATATATATATATATATATATATATAT',
+                'stick_len': 10,
+                'stick_seq': 'GCGCGCGCGCGCGCGCGCGCGCGCGCGC',
+                'type': type,            
             });
         });
-     },
-    _get_next: function(f)
+
+        $(document).on('click', '.join > .seq > .base', function() {
+            $(this)
+            .addClass('sl')
+            .siblings()
+            .removeClass('sl');
+        });
+
+        $(document).on('click', '#right_seq > .base', function() {
+            var t = $(this);
+            self.pview.find('.primer').css({
+                'right': t.offsetParent().width() - 
+                    t.position().left - t.outerWidth(),
+            });
+        });
+        $(document).on('click', '#left_seq > .base', function() {
+            self.pview.find('.primer').css({
+                'left': $(this).position().left,
+            });
+        });
+
+    },
+    _init: function() {
+        this.el.find('.fragment:not(.bk-fragment)').each( function(i) {
+            $(this).find('.seq').css({
+                'background-color': 'hsl('+90*i+', 40%, 40%)',//libFrag.getNextColor(),
+            });
+        });
+
+    },
+    loadJson: function(json)
     {
-        var n = f.next('.fragment');
-        if(n.size() == 0)
+        var p = this.pview.find('.join > .primer').prop('id', 'primer-'+json.id);
+        var l, r, l_seq, r_seq;
+        if(json.type == 'fwd')
         {
-            n = f.siblings('.fragment:not(.bk-fragment)').first();
+            l = json.flap_len;
+            l_seq = json.flap_seq;
+            r = json.stick_len;
+            r_seq = json.stick_seq;            
+            p.removeClass('rev-primer').addClass('fwd-primer');
         }
-        return n;
-    },
-    _get_prev: function(f)
-    {
-        var n = f.prev('.fragment');
-        if(n.hasClass('bk-fragment'))
+        else
         {
-            n = f.siblings('.fragment:not(.bk-fragment)').last();
+            l = json.stick_len;
+            l_seq = json.stick_seq;
+            r = json.flap_len;
+            r_seq = json.flap_seq;
+            p.removeClass('fwd-primer').addClass('rev-primer');
         }
-        return n;
+
+        this._fill(this.pview.find('#left_seq'), l_seq);
+        this._fill(this.pview.find('#right_seq'), r_seq);
+
+        var lb = this.pview.find('.join > #left_seq > [offset='+l+']').addClass('sl');
+        var rb = this.pview.find('.join > #right_seq > [offset='+r+']').addClass('sl');
+
+        console.log('lb.position().left = ' + lb.position().left);
+        console.log('rb.position().left = ' + rb.position().left);
+
+        this.pview.find('.primer').css({'left': lb.position().left,});
+        this.pview.find('.primer').css({
+            'right': rb.offsetParent().width() - rb.position().left - rb.outerWidth(),
+        });    
+
     },
-    showPrimerView: function(f)
+    toJson: function()
     {
-        this.el.find('.primer-view')
-            .slideDown()
-            .children('.arrow')
-            .animate({'left': this._get_center(f)});
-    },
-    hidePrimerView: function()
-    {
-        this.el.find('.primer-view').slideUp();
+        var l = this.pview.find('.join > #left_seq  > .sl').attr('offset');
+        var r = this.pview.find('.join > #right_seq > .sl').attr('offset');
+
+        var s;
+        var f;
+
+        var type;
+
+        if($('.join > .primer').hasClass('fwd-primer'))
+        {
+            f = l;
+            s = r;
+            type = 'fwd';
+        }
+        else
+        {
+            f = r;
+            s = l;
+            type = 'rev';
+        }
+        return {'id': $('.join > .primer').prop('id').split('-')[1],
+            'stick_len': s,
+            'flap_len': f,
+            'type': type,};
     },
     _get_center: function(f)
     {
         return f.offset().left - this.el.offset().left + f.outerWidth();
     },
- });
+    _set_colors: function(p)
+    {
+        var l, r;
+        if(p.hasClass('fwd-primer'))
+        {
+            r = p.siblings('.seq').css('background-color');
+            var prev = p.parent().prev('.fragment:not(.bk-fragment)');
+            if(prev.length == 0)
+                prev = p.parent().siblings('.fragment:not(.bk-fragment)')
+                    .last();
+            l = prev.find('.seq').css('background-color');
+        }
+        else
+        {
+            l = p.siblings('.seq').css('background-color');
+            var next = p.parent().next('.fragment:not(.bk-fragment)');
+            if(next.length == 0)
+                next = p.parent().siblings('.fragment:not(.bk-fragment)')
+                    .first();
+            r = next.find('.seq').css('background-color');
+        }
+
+        this.pview.find('#left_seq').css('background-color', l);
+        this.pview.find('#right_seq').css('background-color', r);
+    },
+    _fill: function($d, seq)
+    {
+        $d.html('');
+        if($d.prop('id') == 'left_seq')
+        {
+            seq = seq.split('').reverse().join('');
+        }
+        for(var b = 0; b < seq.length; b=b+1)
+        {
+            $('<div>')
+            .addClass('base')
+            .text(seq[b])
+            .attr('offset', b+1)
+            .appendTo($d);
+        }
+
+        $('<div>')
+            .css({'clear':'both',})
+            .appendTo($d);
+    },
+});
+
